3e5751375a6f66e1845e624102aa57225dc5e367
thelordzeus <arnav55125@gmail.com>
2023-09-30 18:10:02 +0530

    feat: added hero page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,48 @@
 "use client";
-
-import { Scanner } from "@/components/Scanner";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <h1>Hello there </h1>
+      <div className="bg-white pb-6 sm:pb-8 lg:pb-12">
+        <div className="mx-auto max-w-screen-xl px-4 md:px-8">
+          <section className="min-h-96 relative flex flex-1 shrink-0 items-center justify-center overflow-hidden rounded-lg bg-gray-100 py-16 shadow-lg md:py-20 xl:py-48">
+            <img
+              src="https://images.unsplash.com/photo-1618004652321-13a63e576b80?auto=format&q=75&fit=crop&w=1500"
+              loading="lazy"
+              alt="Photo by Fakurian Design"
+              className="absolute inset-0 h-full w-full object-cover object-center"
+            />
+
+            <div className="absolute inset-0 bg-indigo-500 mix-blend-multiply"></div>
+
+            <div className="relative flex flex-col items-center p-4 sm:max-w-xl">
+              <p className="mb-4 text-center text-lg text-indigo-200 sm:text-xl md:mb-8">
+                Very proud to introduce
+              </p>
+              <h1 className="mb-8 text-center text-4xl font-bold text-white sm:text-5xl md:mb-12 md:text-6xl">
+                Revolutionary way to do call on the web
+              </h1>
+
+              <div className="flex w-full flex-col gap-2.5 sm:flex-row sm:justify-center">
+                <Link
+                  href="/profile"
+                  className="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base"
+                >
+                  Generate QR
+                </Link>
+
+                <Link
+                  href="/reader"
+                  className="inline-block rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base"
+                >
+                  Make Call
+                </Link>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
     </main>
   );
 }
