23bf4b209503e28591419627a1b53812795fd4a8
thelordzeus <arnav55125@gmail.com>
2023-09-30 17:27:00 +0530

    migrated scanner to component

--- app/page.js
+++ app/page.js
@@ -1,44 +1,13 @@
 "use client";
-import { useEffect, useState } from "react";
-
-import { Html5QrcodeScanner } from "html5-qrcode";
 import { QRCodeGenerator } from "@/components/QRCodeGenerator";
+import { Scanner } from "@/components/Scanner";
 
 export default function Home() {
-  const [scanMode, setScanMode] = useState(false);
-  const [scanResult, setScanResult] = useState(null);
-
-  useEffect(() => {
-    const scanner = new Html5QrcodeScanner("reader", {
-      qrbox: {
-        width: 250,
-        height: 250,
-      },
-      fps: 5,
-    });
-    scanner.render(success, error);
-
-    function success(result) {
-      scanner.clear();
-      setScanResult(result);
-    }
-
-    function error(err) {
-      console.log(err);
-    }
-  }, []);
-
   return (
     <main>
       <div>
         <QRCodeGenerator />
-        {scanResult ? (
-          <div>
-            Success: <a href={"http://" + scanResult}>{scanResult}</a>{" "}
-          </div>
-        ) : (
-          <div id="reader"></div>
-        )}
+        <Scanner />
       </div>
     </main>
   );
