83bdf24c21320415490a104c1092ebf315f03c5f
thelordzeus <arnav55125@gmail.com>
2023-09-30 17:46:10 +0530

    feat: updated qr input

--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
 "use client";
-import { QRCodeGenerator } from "@/components/QRCodeGenerator";
+
 import { Scanner } from "@/components/Scanner";
 
 export default function Home() {
   return (
     <main>
       <div>
-        <QRCodeGenerator />
         <Scanner />
       </div>
     </main>
--- components/QRCodeGenerator.jsx
+++ components/QRCodeGenerator.jsx
@@ -9,12 +9,38 @@ export const QRCodeGenerator = () => {
   };
   return (
     <div>
-      <input
-        type="number"
-        onChange={handleChange}
-        className="text-black border-black"
-        placeholder="Enter your number here"
-      />
+      <div>
+        <label htmlFor="email" className="sr-only">
+          Email
+        </label>
+
+        <div className="relative">
+          <input
+            type="number"
+            className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+            placeholder="Enter your phone number"
+            onChange={handleChange}
+          />
+
+          <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4 text-gray-400"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+              />
+            </svg>
+          </span>
+        </div>
+      </div>
+
       <QRCode value={inputValue} />
     </div>
   );
